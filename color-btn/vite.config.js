80fb7d715997fbde7a588cdc05a8fe22eca47815
ingaSiu <ingasiud@gmail.com>
2023-11-29 18:46:26 +0200

    config vite

--- color-btn/vite.config.js
+++ color-btn/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    // this points to the setup file that we created earlier
+    setupFiles: './test/setup.js',
+    // you might want to disable it, if you don't have tests that rely on CSS
+    // since parsing CSS is slow
+    css: true,
+  },
+});
+
