2936922ca3a8e0f5eb9b7debeb86d71ec8508dd5
ingaSiu <ingasiud@gmail.com>
2023-11-29 18:48:32 +0200

    fix location error

--- color-btn/vite.config.js
+++ color-btn/vite.config.js
@@ -7,8 +7,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    // this points to the setup file that we created earlier
-    setupFiles: './test/setup.js',
+    setupFiles: './src/setupTests.js',
     // you might want to disable it, if you don't have tests that rely on CSS
     // since parsing CSS is slow
     css: true,
