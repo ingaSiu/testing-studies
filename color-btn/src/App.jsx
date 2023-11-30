2863139d7a44b9e321e4060f36157a8c14381d8f
ingaSiu <ingasiud@gmail.com>
2023-11-30 15:15:05 +0200

    add disable btn functionality

--- color-btn/src/App.jsx
+++ color-btn/src/App.jsx
@@ -4,13 +4,22 @@ import { useState } from 'react';
 
 const App = () => {
   const [buttonColor, setButtonColor] = useState('red');
+  const [disabled, setDisabled] = useState(false);
 
   const nextColor = buttonColor === 'red' ? 'blue' : 'red';
   return (
     <div>
-      <button className={buttonColor} onClick={() => setButtonColor(nextColor)}>
+      <button className={buttonColor} onClick={() => setButtonColor(nextColor)} disabled={disabled}>
         Change to {nextColor}
       </button>
+      <br />
+      <input
+        type="checkbox"
+        id="disable-button-checkbox"
+        defaultChecked={false}
+        onChange={(e) => setDisabled(e.target.checked)}
+      />
+      <label htmlFor="disable-button-checkbox">Disable Button</label>
     </div>
   );
 };
