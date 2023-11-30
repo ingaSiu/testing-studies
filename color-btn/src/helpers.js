7fc5510a768f223d1b68267e8f0e5487f7aca2b2
ingaSiu <ingasiud@gmail.com>
2023-11-30 18:42:36 +0200

    write a function for titles

---
+++ color-btn/src/helpers.js
@@ -0,0 +1,6 @@
+export const kebabCaseToTitleCase = (colorName) => {
+  const colorWithSpaces = colorName.replaceAll('-', ' ');
+  const colorWithCaps = colorWithSpaces.replace(/\b([a-z])/g, (match) => match.toUpperCase());
+
+  return colorWithCaps;
+};
