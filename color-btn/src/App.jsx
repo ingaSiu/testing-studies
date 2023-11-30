b1008b8c44209f31f586bb6b6a192b4ed4942fd1
ingaSiu <ingasiud@gmail.com>
2023-11-30 14:38:39 +0200

    add btn functionality and style for testing

--- color-btn/src/App.jsx
+++ color-btn/src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css';
 
-function App() {
+import { useState } from 'react';
+
+const App = () => {
+  const [buttonColor, setButtonColor] = useState('red');
+
+  const nextColor = buttonColor === 'red' ? 'blue' : 'red';
   return (
     <div>
-      <button className="red">Change to blue</button>
+      <button className={buttonColor} onClick={() => setButtonColor(nextColor)}>
+        Change to {nextColor}
+      </button>
     </div>
   );
-}
+};
 
 export default App;
