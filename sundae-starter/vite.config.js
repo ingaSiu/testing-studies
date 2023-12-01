37b4c5e71f38ca082c7ec5e453ee1217e0f3e700
ingaSiu <ingasiud@gmail.com>
2023-12-01 10:56:41 +0200

    config vite

--- sundae-starter/vite.config.js
+++ sundae-starter/vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000,
+    // exit if port 3000 is in use (to avoid CORS errors; server expects port 3000)
+    strict: true,
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    // this points to the setup file created earlier
+    setupFiles: './src/setupTests.js',
+    // you might want to disable it, if you don't have tests that rely on CSS
+    // since parsing CSS is slow
+    css: true,
+  },
+});
+
