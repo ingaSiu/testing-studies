8a39c0223bafe047f660e2aad316dca3508fa27a
ingaSiu <ingasiud@gmail.com>
2023-12-01 14:36:04 +0200

    add code for summary form

--- sundae-starter/src/pages/summary/SummaryForm.jsx
+++ sundae-starter/src/pages/summary/SummaryForm.jsx
@@ -1,5 +1,32 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { useState } from 'react';
+
 const SummaryForm = () => {
-  return <div>SummaryForm</div>;
+  const [tcChecked, setTcChecked] = useState(false);
+
+  const checkboxLabel = (
+    <span>
+      I agree to
+      <span style={{ color: 'blue' }}> Terms and Conditions</span>
+    </span>
+  );
+
+  return (
+    <Form>
+      <Form.Group controlId="terms-and-conditions">
+        <Form.Check
+          type="checkbox"
+          checked={tcChecked}
+          onChange={(e) => setTcChecked(e.target.checked)}
+          label={checkboxLabel}
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit" disabled={!tcChecked}>
+        Confirm order
+      </Button>
+    </Form>
+  );
 };
 
 export default SummaryForm;
