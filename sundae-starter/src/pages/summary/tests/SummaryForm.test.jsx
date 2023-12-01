6de469d435170ceed8bf2eb950a225c292e7bb1e
ingaSiu <ingasiud@gmail.com>
2023-12-01 16:29:02 +0200

    write unit tests for popover hover

--- sundae-starter/src/pages/summary/tests/SummaryForm.test.jsx
+++ sundae-starter/src/pages/summary/tests/SummaryForm.test.jsx
@@ -1,6 +1,7 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 
 import SummaryForm from '../SummaryForm';
+import userEvent from '@testing-library/user-event';
 
 test('Initial conditions', () => {
   render(<SummaryForm />);
@@ -12,16 +13,33 @@ test('Initial conditions', () => {
   expect(checkboxElement).not.toBeChecked();
 });
 
-test('Checkbox enables button on first click and disables on second click', () => {
+test('Checkbox enables button on first click and disables on second click', async () => {
+  const user = userEvent.setup();
   render(<SummaryForm />);
 
   const checkboxElement = screen.getByRole('checkbox', { name: /terms and conditions/i });
   const buttonElement = screen.getByRole('button', { name: /confirm order/i });
 
-  fireEvent.click(checkboxElement);
+  await user.click(checkboxElement);
   expect(buttonElement).toBeEnabled();
   expect(checkboxElement).toBeChecked();
 
-  fireEvent.click(checkboxElement);
+  await user.click(checkboxElement);
   expect(buttonElement).toBeDisabled();
+});
+
+test('popover responds to hover', async () => {
+  const user = userEvent.setup();
+  render(<SummaryForm />);
+  // popover starts out hidden (is not in the document)
+  const nullPopover = screen.queryByText(/no ice cream will actually be delivered/i);
+  expect(nullPopover).not.toBeInTheDocument();
+  // popover appears on mouseover of checkbox label
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  await user.hover(termsAndConditions);
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+  // popover desappears when we mouse out
+  await user.unhover(termsAndConditions);
+  expect(popover).not.toBeInTheDocument();
 });
