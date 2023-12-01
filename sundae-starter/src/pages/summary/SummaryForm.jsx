5256feb08c850c7c90025dfbdf7d027b6f2abe45
ingaSiu <ingasiud@gmail.com>
2023-12-01 16:28:46 +0200

    add popover

--- sundae-starter/src/pages/summary/SummaryForm.jsx
+++ sundae-starter/src/pages/summary/SummaryForm.jsx
@@ -1,14 +1,24 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Popover from 'react-bootstrap/Popover';
 import { useState } from 'react';
 
 const SummaryForm = () => {
   const [tcChecked, setTcChecked] = useState(false);
 
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Body>No ice cream will actually be delivered</Popover.Body>
+    </Popover>
+  );
+
   const checkboxLabel = (
     <span>
       I agree to
-      <span style={{ color: 'blue' }}> Terms and Conditions</span>
+      <OverlayTrigger placement="right" overlay={popover}>
+        <span style={{ color: 'blue' }}> Terms and Conditions</span>
+      </OverlayTrigger>
     </span>
   );
 
