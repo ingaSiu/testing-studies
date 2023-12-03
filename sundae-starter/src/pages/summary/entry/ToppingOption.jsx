08260df014d019f765cf5a4d65672aa1e1c27e6b
ingaSiu <ingasiud@gmail.com>
2023-12-03 19:12:36 +0200

    add props to component

--- sundae-starter/src/pages/summary/entry/ToppingOption.jsx
+++ sundae-starter/src/pages/summary/entry/ToppingOption.jsx
@@ -1,5 +1,11 @@
-const ToppingOption = () => {
-  return <div>ToppingOption</div>;
+import Col from 'react-bootstrap/Col';
+
+const ToppingOption = ({ name, imagePath }) => {
+  return (
+    <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
+      <img style={{ width: '75%' }} src={`http://localhost:3030/${imagePath}`} alt={`${name} topping`} />
+    </Col>
+  );
 };
 
 export default ToppingOption;
