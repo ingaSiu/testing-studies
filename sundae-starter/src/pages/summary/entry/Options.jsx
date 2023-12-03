53bb0a8080ac741d88d99717604281caac456bf4
ingaSiu <ingasiud@gmail.com>
2023-12-03 18:26:14 +0200

    get data and map component

--- sundae-starter/src/pages/summary/entry/Options.jsx
+++ sundae-starter/src/pages/summary/entry/Options.jsx
@@ -1,5 +1,29 @@
+import { useEffect, useState } from 'react';
+
+import ScoopOption from './ScoopOption';
+import axios from 'axios';
+
 const Options = ({ optionType }) => {
-  return <div>Options</div>;
+  const [items, setItems] = useState([]);
+
+  // option type is scoops or toppings
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((response) => setItems(response.data))
+      .catch((error) => {
+        // TODO: handle error response
+      });
+  }, [optionType]);
+
+  // TODO: replace 'null' with ToppingOption when available
+  const ItemComponent = optionType === 'scoops' ? ScoopOption : null;
+
+  const optionItems = items.map((item) => (
+    <ItemComponent key={item.name} name={item.name} imagePath={item.imagePath} />
+  ));
+
+  return <div>{optionItems}</div>;
 };
 
 export default Options;
