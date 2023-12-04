b9d57ec0bb236fe72b8cb73f9ea324eb211f2ced
ingaSiu <ingasiud@gmail.com>
2023-12-04 13:33:27 +0200

    write test for error handling

--- sundae-starter/src/pages/entry/tests/OrderEntry.test.jsx
+++ sundae-starter/src/pages/entry/tests/OrderEntry.test.jsx
@@ -1,5 +1,5 @@
-import { HttpResponse, rest } from 'msw';
-import { render, screen } from '@testing-library/react';
+import { HttpResponse, http } from 'msw';
+import { logRoles, render, screen } from '@testing-library/react';
 
 import OrderEntry from '../OrderEntry';
 import { server } from '../../../mocks/server';
@@ -7,22 +7,26 @@ import { server } from '../../../mocks/server';
 test('Handles errors for scoops and toppings routes', async () => {
   // reset handlers
   server.resetHandlers(
-    rest.get('http://localhost:3030/scoops', () => {
+    http.get('http://localhost:3030/scoops', () => {
       return new HttpResponse(null, { status: 500 });
     }),
-    rest.get('http://localhost:3030/toppings', () => {
+    http.get('http://localhost:3030/toppings', () => {
       return new HttpResponse(null, { status: 500 });
     }),
   );
 
+  // const { container } =
   render(<OrderEntry />);
 
   // choosing with query to use:
   // get => we are expecting it to be there right away
   // query => we are expecting it not to be there right away
-  // find => we expevt it to be asynchronously
+  // find => we expect it to be asynchronously
 
-  const alerts = await screen.findAllByRole('alert', { name: 'An unexpected error occurred. Please try again later.' });
+  const alerts = await screen.findAllByRole('alert');
+
+  // using logRoles helps to troubleshoot
+  // logRoles(container);
 
   expect(alerts).toHaveLength(2);
 });
