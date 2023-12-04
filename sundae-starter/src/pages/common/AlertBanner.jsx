002dc01824bcd36d31267c8fdb89a1d15ba80ca9
ingaSiu <ingasiud@gmail.com>
2023-12-04 13:32:17 +0200

    create alert banner component

---
+++ sundae-starter/src/pages/common/AlertBanner.jsx
@@ -0,0 +1,15 @@
+import Alert from 'react-bootstrap/Alert';
+
+const AlertBanner = ({ message, variant }) => {
+  const alertMessage = message || 'An unexpected error occurred. Please try again later.';
+
+  const alertVariant = variant || 'danger';
+
+  return (
+    <Alert variant={alertVariant} style={{ background: 'red' }}>
+      {alertMessage}
+    </Alert>
+  );
+};
+
+export default AlertBanner;
