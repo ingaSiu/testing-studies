885d5474bf4a8274d8175c56336ce6c8a113d722
ingaSiu <ingasiud@gmail.com>
2023-12-05 12:13:52 +0200

    fix spelling error

--- sundae-starter/src/pages/entry/tests/totalUpdates.test.jsx
+++ sundae-starter/src/pages/entry/tests/totalUpdates.test.jsx
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from '../../../test-utils/testing-library-utils';
 
 import Options from '../Options';
 import userEvent from '@testing-library/user-event';
 
+// we test an isolated component so in order to get context here in tests we need to wrap it with context
+// you can wrap it in redux provider, router etc
+//   render(<Options optionType="scoops" />, { wrapper: OrderDetailsProvider });
 test('update scoop subtotal when scoops change', async () => {
   const user = userEvent.setup();
   render(<Options optionType="scoops" />);
@@ -10,14 +13,14 @@ test('update scoop subtotal when scoops change', async () => {
   // make sure total starts out at $0.00
   const scoopsSubtotal = screen.getByText('Scoops total: $', { exact: false });
 
-  expect(scoopsSubtotal).ToHaveTextContent('0.00');
+  expect(scoopsSubtotal).toHaveTextContent('0.00');
   // update vanilla scoops to 1, and check subtotal
 
   const vanillaInput = await screen.findByRole('spinbutton', { name: 'Vanilla' });
 
   await user.clear(vanillaInput);
   await user.type(vanillaInput, '1');
-  expect(scoopsSubtotal).ToHaveTextContent('2.00');
+  expect(scoopsSubtotal).toHaveTextContent('2.00');
 
   // update chocolate csoops to 2 and check subtotal
 
@@ -25,5 +28,5 @@ test('update scoop subtotal when scoops change', async () => {
 
   await user.clear(chocolateInput);
   await user.type(chocolateInput, '2');
-  expect(scoopsSubtotal).ToHaveTextContent('6.00');
+  expect(scoopsSubtotal).toHaveTextContent('6.00');
 });
