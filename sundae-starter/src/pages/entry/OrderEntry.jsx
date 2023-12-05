3dd8b874eb0654bcdc73c05e50940988245b70db
ingaSiu <ingasiud@gmail.com>
2023-12-05 14:26:41 +0200

    update with grand total

--- sundae-starter/src/pages/entry/OrderEntry.jsx
+++ sundae-starter/src/pages/entry/OrderEntry.jsx
@@ -1,10 +1,14 @@
 import Options from './Options';
+import { formatCurrency } from '../../utilities';
+import { useOrderDetails } from '../../contexts/OrderDetails';
 
 const OrderEntry = () => {
+  const { totals } = useOrderDetails();
   return (
     <div>
       <Options optionType="scoops" />
       <Options optionType="toppings" />
+      <h2>Grand total: {formatCurrency(totals.scoops + totals.toppings)}</h2>
     </div>
   );
 };
