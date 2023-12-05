286b8d84dd1454cd323b4ff8b1b798147d338fc2
ingaSiu <ingasiud@gmail.com>
2023-12-05 11:58:28 +0200

    format options

--- sundae-starter/src/pages/entry/Options.jsx
+++ sundae-starter/src/pages/entry/Options.jsx
@@ -5,10 +5,14 @@ import Row from 'react-bootstrap/Row';
 import ScoopOption from './ScoopOption';
 import ToppingOption from './ToppingOption';
 import axios from 'axios';
+import { formatCurrency } from '../../utilities';
+import { pricePerItem } from '../../constants';
+import { useOrderDetails } from '../../contexts/OrderDetails';
 
 const Options = ({ optionType }) => {
   const [items, setItems] = useState([]);
   const [error, setError] = useState(false);
+  const { totals } = useOrderDetails();
 
   // option type is scoops or toppings
   useEffect(() => {
@@ -26,11 +30,22 @@ const Options = ({ optionType }) => {
 
   const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption;
 
+  const title = optionType[0].toUpperCase() + optionType.slice(1).toLowerCase();
+
   const optionItems = items.map((item) => (
     <ItemComponent key={item.name} name={item.name} imagePath={item.imagePath} />
   ));
 
-  return <Row>{optionItems}</Row>;
+  return (
+    <>
+      <h2>{title}</h2>
+      <p>{formatCurrency(pricePerItem[optionType])} each</p>
+      <p>
+        {title} total: {formatCurrency(totals[optionType])}{' '}
+      </p>
+      <Row>{optionItems}</Row>
+    </>
+  );
 };
 
 export default Options;
