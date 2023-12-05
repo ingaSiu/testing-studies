25acc4d8162963fd9f70ec9a413a00fc37addeee
ingaSiu <ingasiud@gmail.com>
2023-12-05 13:33:19 +0200

    write tests for topping options subtotal

--- sundae-starter/src/pages/entry/tests/totalUpdates.test.jsx
+++ sundae-starter/src/pages/entry/tests/totalUpdates.test.jsx
@@ -6,6 +6,7 @@ import userEvent from '@testing-library/user-event';
 // we test an isolated component so in order to get context here in tests we need to wrap it with context
 // you can wrap it in redux provider, router etc
 //   render(<Options optionType="scoops" />, { wrapper: OrderDetailsProvider });
+
 test('update scoop subtotal when scoops change', async () => {
   const user = userEvent.setup();
   render(<Options optionType="scoops" />);
@@ -22,11 +23,34 @@ test('update scoop subtotal when scoops change', async () => {
   await user.type(vanillaInput, '1');
   expect(scoopsSubtotal).toHaveTextContent('2.00');
 
-  // update chocolate csoops to 2 and check subtotal
+  // update chocolate scoops to 2 and check subtotal
 
   const chocolateInput = await screen.findByRole('spinbutton', { name: 'Chocolate' });
 
   await user.clear(chocolateInput);
   await user.type(chocolateInput, '2');
   expect(scoopsSubtotal).toHaveTextContent('6.00');
+});
+
+test('update toppings subtotal when toppings change', async () => {
+  const user = userEvent.setup();
+  render(<Options optionType="toppings" />);
+
+  const toppingsSubtotal = screen.getByText('Toppings total: $', { exact: false });
+  expect(toppingsSubtotal).toHaveTextContent('0.00');
+
+  const cherriesCheckbox = await screen.findByRole('checkbox', { name: 'Cherries' });
+
+  await user.click(cherriesCheckbox);
+
+  expect(toppingsSubtotal).toHaveTextContent('1.50');
+  expect(cherriesCheckbox).toBeChecked();
+
+  const hotFudgeCheckbox = await screen.findByRole('checkbox', { name: 'Hot fudge' });
+  await user.click(hotFudgeCheckbox);
+  expect(toppingsSubtotal).toHaveTextContent('3.00');
+
+  await user.click(hotFudgeCheckbox);
+  expect(hotFudgeCheckbox).not.toBeChecked();
+  expect(toppingsSubtotal).toHaveTextContent('1.50');
 });
