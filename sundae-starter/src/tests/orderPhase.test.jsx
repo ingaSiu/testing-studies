768dd34248da7ab80c1edbed3f9a58d21c03c3b3
ingaSiu <ingasiud@gmail.com>
2023-12-06 17:51:10 +0200

    some tests added

--- sundae-starter/src/tests/orderPhase.test.jsx
+++ sundae-starter/src/tests/orderPhase.test.jsx
@@ -3,10 +3,27 @@ import { render, screen } from '@testing-library/react';
 import App from '../App';
 import { userEvent } from '@testing-library/user-event';
 
-test('order phases for happy path', () => {
+test('order phases for happy path', async () => {
+  const user = userEvent.setup();
   // render app
+  render(<App />);
+
   // add ice cream scoops and toppings
-  // find and click order button
+  const grandTotal = screen.getByRole('heading', { name: /Grand total: \$/i });
+  expect(grandTotal).toHaveTextContent('0.00');
+
+  const vanillaInput = await screen.findByRole('spinbutton', { name: 'Vanilla' });
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, '1');
+
+  const cherriesCheckbox = await screen.findByRole('checkbox', { name: 'Cherries' });
+  await user.click(cherriesCheckbox);
+  expect(grandTotal).toHaveTextContent('3.50');
+
+  // find and click order summary button
+  const orderSummaryBtn = screen.getByRole('button', { name: /order sundae/i });
+  await user.click(orderSummaryBtn);
+
   // check summary information based on order
   // accept terms and conditions and click button to confirm order
   // confirm order number on confirmation page
