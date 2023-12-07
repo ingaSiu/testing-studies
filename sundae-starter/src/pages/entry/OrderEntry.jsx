7163503b9369dca8e737512aa2a58da7b62523a4
ingaSiu <ingasiud@gmail.com>
2023-12-07 18:30:33 +0200

    add button element for order

--- sundae-starter/src/pages/entry/OrderEntry.jsx
+++ sundae-starter/src/pages/entry/OrderEntry.jsx
@@ -1,14 +1,21 @@
+import Button from 'react-bootstrap/Button';
 import Options from './Options';
 import { formatCurrency } from '../../utilities';
 import { useOrderDetails } from '../../contexts/OrderDetails';
 
-const OrderEntry = () => {
+const OrderEntry = ({ setOrderPhase }) => {
   const { totals } = useOrderDetails();
+
+  // disable order button if there aren't any scoops in order
+  const orderDisabled = totals.scoops === 0;
   return (
     <div>
       <Options optionType="scoops" />
       <Options optionType="toppings" />
       <h2>Grand total: {formatCurrency(totals.scoops + totals.toppings)}</h2>
+      <Button disabled={orderDisabled} onClick={() => setOrderPhase('review')}>
+        Order Sundae!
+      </Button>
     </div>
   );
 };
