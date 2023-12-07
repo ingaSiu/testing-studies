fae7dd42c9aa77d9b82fabf69307ca9778757d3c
ingaSiu <ingasiud@gmail.com>
2023-12-07 18:27:57 +0200

    update app with order phase

--- sundae-starter/src/App.jsx
+++ sundae-starter/src/App.jsx
@@ -1,15 +1,33 @@
 import Container from 'react-bootstrap/Container';
+import OrderConfirmation from './confirmation/OrderConfirmation';
 import { OrderDetailsProvider } from './contexts/OrderDetails';
 import OrderEntry from './pages/entry/OrderEntry';
+import OrderSummary from './pages/summary/OrderSummary';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [orderPhase, setOrderPhase] = useState('inProgress');
+
+  let Component = OrderEntry; // default to order page
+  switch (orderPhase) {
+    case 'inProgress':
+      Component = OrderEntry;
+      break;
+    case 'review':
+      Component = OrderSummary;
+      break;
+    case 'completed':
+      Component = OrderConfirmation;
+      break;
+    default:
+  }
   return (
     <Container>
       <OrderDetailsProvider>
-        <OrderEntry />
+        <Container>{<Component setOrderPhase={setOrderPhase} />}</Container>
       </OrderDetailsProvider>
     </Container>
   );
-}
+};
 
 export default App;
