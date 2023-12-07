8ab5c3ba5ad0ae17ac47eef236d13497abb24239
ingaSiu <ingasiud@gmail.com>
2023-12-07 18:28:36 +0200

    write test for handling error resp in order confirmation

---
+++ sundae-starter/src/confirmation/tests/OrderConfirmation.test.jsx
@@ -0,0 +1,19 @@
+import { HttpResponse, http } from 'msw';
+import { render, screen } from '../../test-utils/testing-library-utils';
+
+import OrderConfirmation from '../OrderConfirmation';
+import { server } from '../../mocks/server';
+
+test('error response from server for submitting order', async () => {
+  // override default msw response for options endpoint with error response
+  server.resetHandlers(
+    http.post('http://localhost:3030/order', () => {
+      return new HttpResponse(null, { status: 500 });
+    }),
+  );
+
+  render(<OrderConfirmation />);
+
+  const alert = await screen.findAllByRole('alert');
+  expect(alert).toHaveTextContent('An unexpected error occurred. Please try again later.');
+});
