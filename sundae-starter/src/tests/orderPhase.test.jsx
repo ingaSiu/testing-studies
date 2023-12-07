96d92147c1d482ac65ed183fdd53d89c412c2ce6
ingaSiu <ingasiud@gmail.com>
2023-12-07 18:27:37 +0200

    write all tests for happy path

--- sundae-starter/src/tests/orderPhase.test.jsx
+++ sundae-starter/src/tests/orderPhase.test.jsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import App from '../App';
 import { userEvent } from '@testing-library/user-event';
 
-test('order phases for happy path', async () => {
+test('Order phases for happy path', async () => {
   const user = userEvent.setup();
   // render app
   render(<App />);
@@ -24,10 +24,61 @@ test('order phases for happy path', async () => {
   const orderSummaryBtn = screen.getByRole('button', { name: /order sundae/i });
   await user.click(orderSummaryBtn);
 
-  // check summary information based on order
+  // check summary subtotals
+  const summaryHeading = screen.getByRole('heading', { name: 'Order Summary' });
+  expect(summaryHeading).toBeInTheDocument();
+
+  const scoopsHeading = screen.getByRole('heading', { name: 'Scoops: $2.00' });
+  expect(scoopsHeading).toBeInTheDocument();
+
+  const toppingsHeading = screen.getByRole('heading', { name: 'Toppings: $1.50' });
+  expect(toppingsHeading).toBeInTheDocument();
+
+  // check summary option items
+  expect(screen.getByText('1 Vanilla')).toBeInTheDocument();
+  expect(screen.getByText('Cherries')).toBeInTheDocument();
+
+  // alternatively:
+  // const optionItems = screen.getAllByRole("listitem");
+  // const optionItemsText = optionItems.map((item) => item.textContent);
+  // expect(optionItemsText).toEqual(['1 Vanilla', "Cherries"])
+
   // accept terms and conditions and click button to confirm order
+  const cofirmOrderBtn = screen.getByRole('button', { name: /confirm order/i });
+  expect(cofirmOrderBtn).toBeDisabled();
+
+  const tcCheckBox = screen.getByRole('checkbox', { name: /I agree to terms and conditions/i });
+  await user.click(tcCheckBox);
+
   // confirm order number on confirmation page
+  expect(cofirmOrderBtn).toBeEnabled();
+  await user.click(cofirmOrderBtn);
+
+  // Expect "loading" to show
+  const loading = screen.getByText(/loading/i);
+  expect(loading).toBeInTheDocument();
+
+  // check confirmation page text
+  // this one is async because there is a POST request to server in between summary
+  // and comfirmations pages
+
+  const thankYouHeader = await screen.findByRole('heading', { name: /thank you/i });
+  expect(thankYouHeader).toBeInTheDocument();
+
+  // expect that loading has disappeared
+  const notLoading = screen.queryByText('loading');
+  expect(notLoading).not.toBeInTheDocument();
+
+  const orderNumber = await screen.findByText(/order number/i);
+  expect(orderNumber).toBeInTheDocument();
+
   // click "new order" button on conformation page
+  const newOrderBtn = screen.getByRole('button', { name: /create new order/i });
+  await user.click(newOrderBtn);
+
   // check that scoops and toppings subtotal have been reset
-  // do we need await anything to avaoid tests errors?
+  const scoopsTotal = await screen.findByText('Scoops total: $0.00');
+  expect(scoopsTotal).toBeInTheDocument();
+  const toppingsTotal = await screen.findByText('Toppings total: $0.00');
+  expect(toppingsTotal).toBeInTheDocument();
 });
