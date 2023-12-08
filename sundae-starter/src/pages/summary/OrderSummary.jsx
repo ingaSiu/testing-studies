8c349100d52011258984d26aea70c1f2dd51bd41
ingaSiu <ingasiud@gmail.com>
2023-12-08 09:33:57 +0200

    display toppings if total toppings are more than zero

--- sundae-starter/src/pages/summary/OrderSummary.jsx
+++ sundae-starter/src/pages/summary/OrderSummary.jsx
@@ -13,17 +13,28 @@ const OrderSummary = ({ setOrderPhase }) => {
     </li>
   ));
 
-  const toppingArray = Object.keys(optionCounts.toppings);
-  const toppingList = toppingArray.map((key) => <li key={key}>{key}</li>);
+  // only display toppings if the toppings total is nonzero
+
+  const hasToppings = totals.toppings > 0;
+  let toppingsDisplay = null;
+
+  if (hasToppings) {
+    const toppingArray = Object.keys(optionCounts.toppings);
+    const toppingList = toppingArray.map((key) => <li key={key}>{key}</li>);
+    toppingsDisplay = (
+      <>
+        <h2>Toppings: {formatCurrency(totals.toppings)}</h2>
+        <ul>{toppingList}</ul>
+      </>
+    );
+  }
 
   return (
     <div>
       <h1>Order Summary</h1>
       <h2>Scoops: {formatCurrency(totals.scoops)}</h2>
       <ul>{scoopList}</ul>
-
-      <h2>Toppings: {formatCurrency(totals.toppings)}</h2>
-      <ul>{toppingList}</ul>
+      {toppingsDisplay}
       <SummaryForm setOrderPhase={setOrderPhase} />
     </div>
   );
