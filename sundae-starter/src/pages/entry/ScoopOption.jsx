dc6874dfb2d2dffafcd772c8805ecf30dd2aa9f0
ingaSiu <ingasiud@gmail.com>
2023-12-08 12:49:25 +0200

    update scoop subtotal if value is not valid/ valid

--- sundae-starter/src/pages/entry/ScoopOption.jsx
+++ sundae-starter/src/pages/entry/ScoopOption.jsx
@@ -21,7 +21,10 @@ const ScoopOption = ({ name, imagePath }) => {
     // validate
     setIsValid(valueIsValid);
 
-    updateItemCount(name, parseInt(currentValue), 'scoops');
+    // update scoop count to 0 if value is in invalid
+    const newValue = valueIsValid ? parseInt(currentValue) : 0;
+
+    updateItemCount(name, newValue, 'scoops');
   };
 
   return (
