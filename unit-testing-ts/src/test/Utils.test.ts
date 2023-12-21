7fb0778da01108642cc593cd8281c57455d72041
ingaSiu <ingasiud@gmail.com>
2023-12-21 15:12:40 +0200

    added example of proper structure unit test

--- unit-testing-ts/src/test/Utils.test.ts
+++ unit-testing-ts/src/test/Utils.test.ts
@@ -1,8 +1,16 @@
 import { toUpperCase } from '../app/Utils';
 
 describe('Utils test suite', () => {
-  test('Should return uppercase', () => {
-    const result = toUpperCase('abc');
-    expect(result).toBe('ABC');
+  // example of a proper structure unit test
+  it('Should return uppercase of valid string', () => {
+    // arrange
+    const sut = toUpperCase;
+    const expected = 'ABC';
+
+    // act: (result)
+    const actual = toUpperCase('abc');
+
+    // assert
+    expect(actual).toBe(expected);
   });
 });
