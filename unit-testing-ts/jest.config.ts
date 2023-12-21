3e0164804ef59be6da6cf07db15759d373795ef1
ingaSiu <ingasiud@gmail.com>
2023-12-21 18:53:21 +0200

    add code coverage config

--- unit-testing-ts/jest.config.ts
+++ unit-testing-ts/jest.config.ts
@@ -4,6 +4,9 @@ const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/app/**/*.ts'],
 };
 
 export default config;
+
