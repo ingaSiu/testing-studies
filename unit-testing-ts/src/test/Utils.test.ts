0c8b52230617cd90a1c32b77da19fc777608e143
ingaSiu <ingasiud@gmail.com>
2023-12-21 15:32:20 +0200

    add more examples of jest assertions and matchers

--- unit-testing-ts/src/test/Utils.test.ts
+++ unit-testing-ts/src/test/Utils.test.ts
@@ -1,4 +1,4 @@
-import { toUpperCase } from '../app/Utils';
+import { getStringInfo, toUpperCase } from '../app/Utils';
 
 describe('Utils test suite', () => {
   // example of a proper structure unit test
@@ -12,5 +12,28 @@ describe('Utils test suite', () => {
 
     // assert
     expect(actual).toBe(expected);
+  });
+
+  it.only('Should return information for valid string', () => {
+    const actual = getStringInfo('My-String');
+
+    // use toBe then comparing primitive values
+    expect(actual.lowerCase).toBe('my-string');
+    // then comparing objects use toEqual
+    expect(actual.extraInfo).toEqual({});
+
+    expect(actual.characters.length).toBe(9);
+    expect(actual.characters).toHaveLength(9);
+
+    expect(actual.characters).toEqual(['M', 'y', '-', 'S', 't', 'r', 'i', 'n', 'g']);
+    expect(actual.characters).toContain<string>('M');
+
+    expect(actual.characters).toEqual(expect.arrayContaining(['S', 't', 'r', 'i', 'n', 'g', 'M', 'y', '-']));
+
+    // check object definition
+    expect(actual.extraInfo).not.toBe(undefined);
+    expect(actual.extraInfo).not.toBeUndefined();
+    expect(actual.extraInfo).toBeDefined();
+    expect(actual.extraInfo).toBeTruthy(); // can check for defined or undefined objects if not sure about their struicture
   });
 });
