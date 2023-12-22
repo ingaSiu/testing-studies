ec24ca6a413219290d4f0508b30b82adf2795668
ingaSiu <ingasiud@gmail.com>
2023-12-22 12:59:56 +0200

    first iteration for password checker

--- unit-testing-ts/src/test/pass_checker/PasswordChecker.test.ts
+++ unit-testing-ts/src/test/pass_checker/PasswordChecker.test.ts
@@ -1,5 +1,7 @@
 import { PasswordChecker } from '../../app/pass_checker/PasswordChecker';
 
+// SUT - system under test
+
 describe('Password checker test suite', () => {
   let sut: PasswordChecker;
 
@@ -7,7 +9,33 @@ describe('Password checker test suite', () => {
     sut = new PasswordChecker();
   });
 
-  it('Should do nothing', () => {
-    sut.checkPassword();
+  it('Password with less than 8 characters is invalid ', () => {
+    const actual = sut.checkPassword('2352542');
+    expect(actual).toBe(false);
+  });
+
+  it('Password with more than 8 characters is OK', () => {
+    const actual = sut.checkPassword('12345678Aa');
+    expect(actual).toBe(true);
+  });
+
+  it('Password with no uppercase letter is invalid', () => {
+    const actual = sut.checkPassword('12345abcd');
+    expect(actual).toBe(false);
+  });
+
+  it('Password with uppercase letter is valid', () => {
+    const actual = sut.checkPassword('12345Abcd');
+    expect(actual).toBe(true);
+  });
+
+  it('Password without lowercase letter is invalid', () => {
+    const actual = sut.checkPassword('12345ABCD');
+    expect(actual).toBe(false);
+  });
+
+  it('Password with lowercase letter is valid', () => {
+    const actual = sut.checkPassword('12345ABcD');
+    expect(actual).toBe(true);
   });
 });
