8b5ce0c916568a629c6bd031bb6bc2322b33b33a
ingaSiu <ingasiud@gmail.com>
2023-12-23 17:30:17 +0200

    stub example

---
+++ unit-testing-ts/src/test/doubles/OtherUtils.test.ts
@@ -0,0 +1,16 @@
+import { calculateComplexity } from '../../app/doubles/OtherUtils';
+
+describe('OtherUtils test suite', () => {
+  it('Calculates complexity', () => {
+    const someInfo = {
+      length: 5,
+      extraInfo: {
+        field1: 'someInfo',
+        field2: 'someOtherInfo',
+      },
+    };
+
+    const actual = calculateComplexity(someInfo);
+    expect(actual).toBe(10);
+  });
+});
