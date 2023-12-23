e731db7ef8dde293f41ef6cc2dae7fa07bf9bcc7
ingaSiu <ingasiud@gmail.com>
2023-12-23 17:54:18 +0200

    add another func for tests

--- unit-testing-ts/src/app/doubles/OtherUtils.ts
+++ unit-testing-ts/src/app/doubles/OtherUtils.ts
@@ -6,6 +6,18 @@ export type stringInfo = {
   extraInfo: Object | undefined;
 };
 
+type LoggerServiceCallback = (arg: string) => void;
+
 export const calculateComplexity = (stringInfo: stringInfo) => {
   return Object.keys(stringInfo.extraInfo).length * stringInfo.length;
+};
+
+export const toUpperCaseWithCallback = (arg: string, callBack: LoggerServiceCallback) => {
+  if (!arg) {
+    callBack('Invalid argument!');
+    return;
+  }
+  callBack(`called function with ${arg}`);
+
+  return arg.toUpperCase();
 };
