19d239f91047e90055c68c1b4c05059f179f76bc
ingaSiu <ingasiud@gmail.com>
2024-01-03 10:32:16 +0200

    add different code for test to pass

--- redux-testing/client/src/features/toast/redux/LogErrorToastSaga.ts
+++ redux-testing/client/src/features/toast/redux/LogErrorToastSaga.ts
@@ -1,27 +1,30 @@
-import { PayloadAction } from "@reduxjs/toolkit";
-import { SagaIterator } from "redux-saga";
-import { call, put, takeEvery } from "redux-saga/effects";
+import { call, put, takeEvery } from 'redux-saga/effects';
 
-import { ToastOptions } from "../types";
-import { showToast } from "./toastSlice";
+import { PayloadAction } from '@reduxjs/toolkit';
+import { SagaIterator } from 'redux-saga';
+import { ToastOptions } from '../types';
+import { showToast } from './toastSlice';
 
-// presumably this would send the toast to some analytics engine
-const logErrorToast = (title: string) => {
-  // eslint-disable-next-line no-console
-  console.error("Got error toast!", title);
+// // presumably this would send the toast to some analytics engine
+// export const logErrorToast = (title: string) => {
+//   // eslint-disable-next-line no-console
+//   console.error('Got error toast!', title);
+// };
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const sendToAnalytics = (title: string): void => {
+  // presumably this would send the event to some analytics engine
 };
 
-export function* logErrorToasts({
-  payload,
-}: PayloadAction<ToastOptions>): SagaIterator {
+export function* logErrorToast({ payload }: PayloadAction<ToastOptions>): SagaIterator {
   const { title, status } = payload;
-  if (status === "error") {
-    yield call(logErrorToast, title);
+  if (status === 'error') {
+    yield call(sendToAnalytics, title);
   }
   yield put(showToast({ title, status }));
 }
 
-// not very useful, didn't bother adding to root saga
 export function* watchToasts(): SagaIterator {
-  yield takeEvery(showToast.type, logErrorToasts);
+  yield takeEvery(startToast.type, logErrorToast);
 }
+
